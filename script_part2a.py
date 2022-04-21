099523ce3447a375989a36caddd9b8d48c65e0f2
Arne022 <102752572+Arne022@users.noreply.github.com>
2022-04-21 15:40:27 +0200

    Update script_part2a.py

--- script_part2a.py
+++ script_part2a.py
@@ -1,9 +1,8 @@
-#script part2a
+#script part2a, Sammy, Nikolai, Aron
 import roadster
 import numpy as np
 
 route_dist, route_speed = roadster.load_route('speed_elsa.npz')
-#a = roadster.time_to_destination(10, 'speed_anna.npz', 10)
-a = roadster.total_consumption(65,"speed_anna.npz",10000)
-print(a)
-
+distans = route_dist[-1]
+a = roadster.time_to_destination(distans, 'speed_elsa.npz', 10000)
+print('Det tog', a * 60, 'minuter att köra', distans, 'km')
